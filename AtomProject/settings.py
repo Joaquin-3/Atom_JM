6e316e61a7a13cba1bbdca721775e976e8e2a3d4
Joaquin <joaquin.molina18@inacapmail.cl>
2025-12-23 03:31:44 -0300

    c

--- AtomProject/settings.py
+++ AtomProject/settings.py
@@ -133,11 +133,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = MEDIA_DIR
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [STATIC_DIR]
+if DEBUG:
+    STATICFILES_DIRS = [STATIC_DIR]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
